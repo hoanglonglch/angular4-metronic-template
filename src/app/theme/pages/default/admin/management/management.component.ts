dd7d972c9529d2ddcb345e18959d4e600a90e828
larry <larry@enclave.vn>
2018-01-23 10:53:08 +0700

    before apply primeNg

--- src/app/theme/pages/default/admin/management/management.component.ts
+++ src/app/theme/pages/default/admin/management/management.component.ts
@@ -1,15 +1,48 @@
-import {Component, OnInit, ViewEncapsulation} from '@angular/core';
+import {AfterViewInit, Component, OnInit, ViewEncapsulation} from '@angular/core';
+import {ScriptLoaderService} from "../../../../../_services/script-loader.service";
 
 @Component({
     selector: 'app-admin-management',
     templateUrl: "./management.component.html",
     encapsulation: ViewEncapsulation.None,
 })
-export class ManagementComponent implements OnInit {
+export class ManagementComponent implements OnInit, AfterViewInit{
 
-  constructor() { }
+  userDatas: any [];
 
-  ngOnInit() {
+  dummyData = [
+    {
+      username:'larry',
+      role: 'admin',
+    },
+    {
+      username:'larry1',
+      role: 'admin1',
+    },
+    {
+      username:'larry2',
+      role: 'admin2',
+    },
+    {
+      username:'larry3',
+      role: 'admin3',
+    },
+    {
+      username:'larry4',
+      role: 'admin4',
+    },
+  ];
+
+  constructor(private _script: ScriptLoaderService)  {
+
+  }
+  ngOnInit()  {
+    this.userDatas = this.dummyData;
   }
 
+  ngAfterViewInit()  {
+    /*this._script.load('.m-grid__item.m-grid__item--fluid.m-wrapper',
+      'assets/app/js/management.js');*/
+
+  }
 }
